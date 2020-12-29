b81f71ec72ca2bfd4018d558a7bab589649aeeab
Duc Dinh <holahu9@gmail.com>
2020-12-29 09:41:34 -0500

    update script

---
+++ asset/script.js
@@ -0,0 +1,25 @@
+
+//apikey
+ var APIkey = "fc3b2b9467a199bbf230acad7fb52d95";
+
+//create an empty array which will store city's name later
+var city = [];
+
+
+// access to elements
+
+var searchHistoryList = $('#search-history-list');
+var searchCityInput = $("#search-city");
+var searchCityButton = $("#search-city-button");
+var clearHistoryButton = $("#clear-history");
+
+var currentCity = $("#current-city");
+var currentTemp = $("#current-temp");
+var currentHumidity = $("#current-humidity");
+var currentWindSpeed = $("#current-wind-speed");
+var UVindex = $("#uv-index");
+
+var weatherContent = $("#weather-content");
+
+
+
